8ef120ac770b211b1f0e867eb2643bfa86fe472a
silviawolves <silviacanepa.w@gmail.com>
2022-05-30 14:58:39 +0200

    generato numero random per bombe

--- script.js
+++ script.js
@@ -3,14 +3,29 @@
 //recupero elemento HTML in cui inserire le celle
 const containerCampoMinato = document.querySelector('.campo-minato')
 
+//creo la funzione per generare le bombe, a seconda di quante celle avrò nel campo minato
+function celleBombe(max) {
+    const bombeCampo = []
+
+    for (let i = 1; i <= 16; i++) {
+        const indiciBombe = Math.floor(Math.random() * max + 1)
+
+        if (!bombeCampo.includes(indiciBombe)) {
+            bombeCampo.push(indiciBombe)
+        }
+    }
+    return bombeCampo
+}
+
+console.log(celleBombe(49))
+
 //creo una funzione generica per creare X celle in base al livello di difficolta
 //ora settata a 1 = 100 celle
 function campoMinato(cellaX, cellaY) {
-
     //cosa stabilisce la funzione in sé
     const celleRisultanti = cellaX * cellaY
 
-    //assegno una dimensione che aggiusti la quantità di caselle per row
+    //assegno una dimensione che aggiusti la quantità di caselle per row, a prescindere da quante celle genero
     containerCampoMinato.style.width = `calc((var(--size) * ${cellaX}) + (2 * var(--border))`
 
     //creo un ciclo per formare ogni singola cella
@@ -18,18 +33,17 @@ function campoMinato(cellaX, cellaY) {
         const cella = document.createElement('div')
         cella.classList.add('cella')
         containerCampoMinato.append(cella)
-
-        //dentro ogni cella le stampo il suo indice, per ottenere i numeri incrementali (che sono l'i)
-        cella.innerHTML = `<span>${i}</span>`
+        cella.dataset.indice = i
 
         //aggiunto un eventListener per cambiare colore alla cella quando clicco
         cella.addEventListener ('click', function(){
             this.classList.toggle('on-click')
-            console.log("hai cliccato sul numero", this.innerText)
+            console.log("hai cliccato sul numero", i)
         })
     }
 }
 
+//creo la sezione per settare la difficoltà del gioco
 //recupero le informazioni del bottone e del select
 const bottonePlay = document.getElementById("bottone-play")
 
@@ -40,16 +54,19 @@ bottonePlay.addEventListener ('click', function() {
     //a seconda dell'opzione selezionata, le caselle generate cambiano
     if (document.getElementById('1').selected) {
         campoMinato(10, 10)
+        celleBombe(100)
     } else if (document.getElementById('2').selected) {
         campoMinato(9, 9)
+        celleBombe(81)
     } else if (document.getElementById('3').selected) {
         campoMinato(7, 7)
+        celleBombe(49)
     }
 })
 
 
 /*
-Il computer deve generare 16 numeri casuali compresi nel range della griglia: le bombe.I numeri nella lista delle bombe non possono essere duplicati.
+Il computer deve generare 16 numeri casuali compresi nel range della griglia: le bombe. I numeri nella lista delle bombe non possono essere duplicati.
 
 In seguito l’utente clicca su una cella:
 se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina,
